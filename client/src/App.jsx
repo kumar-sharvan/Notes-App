a6109bb8f90cbb911327b5f41dddc9dccc82dca9
Sharvan Kumar <162790236+kumar-sharvan@users.noreply.github.com>
2025-02-15 13:12:05 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,18 +7,20 @@ import Notes from "./Pages/Notes";
 import Home from "./Pages/Home";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/notes" element={<Notes />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<Navbar/>} />
+          <Route path="/signup" element={<SignUp/>} />
+          <Route path="/signin" element={<SignIn/>} />
+          <Route path="/notes" element={<Notes/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
       </div>
     </BrowserRouter>
